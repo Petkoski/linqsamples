88b4af837854d6f1e2cb0504bb2b57920cc348bb
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-20 11:04:25 +0200

    Exceptions and deferred queries CH04-07

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -27,13 +27,26 @@ namespace Queries
 
             //Turn off deferred execution and do something that immediately executes that query
             //and materialize it into a CONCRETE RESULT (ToArray, ToList, ToDictionary).
-            var query = movies.Filter(m => m.Year > 2000).ToList();
+            //var query = movies.Filter(m => m.Year > 2000).ToList();
+
+            var query = Enumerable.Empty<Movie>();
+
+            //Exceptions and deferred queries (CH04-07)
+            try
+            {
+                query = movies.Filter(m => m.Year > 2000); //Without .ToList(), we are just defining the query here. The exception is thrown in the Count() below.
+                //Bottom line is that we really need to use try..catch block in the code that EXECUTES the query.
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             //foreach (var movie in query)
             //{
             //    Console.WriteLine(movie.Title);
             //}
-            
+
             Console.WriteLine(query.Count()); //Forces the query to execute immediately, so that the Count() operator can loop through the results
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext())
